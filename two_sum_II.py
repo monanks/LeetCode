e335e0e6320cb08907f3baf3168b0a5d2c8de36a
monank savliya <monank.savaliya@sjsu.edu>
2018-01-07 13:29:05 -0800

    Two Sum II

---
+++ two_sum_II.py
@@ -0,0 +1,13 @@
+import itertools
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        look = {}
+        for i,num in enumerate(numbers):
+            if target-numbers[i] in look:
+                return [look[target - num],i+1]
+            look[num] = i+1
