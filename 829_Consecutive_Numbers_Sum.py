f7d1e70cf822621e5b397ad0623196364ba4af89
monanks <mcsavaliya.7401@gmail.com>
2018-05-09 13:38:33 -0700

    update

--- 829_Consecutive_Numbers_Sum.py
+++ 829_Consecutive_Numbers_Sum.py
@@ -4,15 +4,29 @@ class Solution(object):
         :type N: int
         :rtype: int
         """
-        count = 0
-        for i in range(1,N+1):
-            i *= 1.0
-            if (N/i)-(i/2) < 0:
-                break
-            if (N/i)%1 == 0 and i%2 == 1:
+        #method 2
+        
+        count = 1
+        t = math.sqrt(2*N)
+        i = 2
+        while i < t:
+            if ( N - ( i * (i -1) / 2 )) % i == 0:
                 count += 1
-            elif (N/i)%1 == 0.5 and i%2 == 0:
-                count += 1
-            
+            i += 1
+
         return count
+        
+#         #method 1
+#         count = 0
+#         i = 1.0
+#         while(1):
+#             if (N/i)-(i/2) < 0:
+#                 break
+#             if (N/i)%1 == 0 and i%2 == 1:
+#                 count += 1
+#             elif (N/i)%1 == 0.5 and i%2 == 0:
+#                 count += 1
+#             i += 1
+            
+#         return count
         
